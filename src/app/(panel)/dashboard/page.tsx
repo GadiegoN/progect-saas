8d74c66e60e9ce8ce275267acb0df7e4ffba3638
GadiegoN <ngadiego@gmail.com>
2025-05-10 10:39:50 -0300

    feat: update dependencies and add new UI components
    - Added new dependencies: @radix-ui/react-dialog, class-variance-authority, clsx, lucide-react, and tailwind-merge.
    - Removed unused dependency from package.json.
    - Added new images: doctor-hero.png, foto1.png, and logo-odonto.png.
    - Created a new Dashboard page component.
    - Created a new Home page component.
    - Updated globals.css with new CSS variables and dark mode styles.
    - Deleted old Home page component.
    - Implemented a Sheet component with various subcomponents for UI.
    - Added utility function for class name merging.

---
+++ src/app/(panel)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello Dashboard!</h1>
+    </main>
+  );
+}
---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-between min-h-screen pt-24">
+      <h1>Hello World</h1>
+    </main>
+  );
+}
