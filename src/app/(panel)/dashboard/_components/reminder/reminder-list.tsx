c8509ccb83e49b0019685039621ef80137be5731
GadiegoN <ngadiego@gmail.com>
2025-06-11 20:18:39 -0300

    feat: implement delete reminder functionality with error handling and user feedback

--- src/app/(panel)/dashboard/_components/reminder/reminder-list.tsx
+++ src/app/(panel)/dashboard/_components/reminder/reminder-list.tsx
@@ -5,12 +5,26 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Reminder } from "@/generated/prisma";
 import { Plus, Trash2 } from "lucide-react";
+import { deleteReminder } from "./_actions/delete-reminder";
+import { toast } from "sonner";
 
 interface ReminderListProps {
   reminder: Reminder[];
 }
 
 export function ReminderList({ reminder }: ReminderListProps) {
+  async function handleDeleteReminder(id: string) {
+    const response = await deleteReminder({ reminderId: id });
+
+    if (response.error) {
+      toast.error(response.error);
+
+      return;
+    }
+
+    toast.success(response.data);
+  }
+
   return (
     <div className="flex flex-col gap-3">
       <Card>
@@ -29,18 +43,26 @@ export function ReminderList({ reminder }: ReminderListProps) {
             </article>
           )}
 
-          {reminder.map((item) => (
-            <article
-              key={item.id}
-              className="flex flex-wrap flex-row items-center justify-between py-2 bg-muted mb-2 px-2 rounded-md"
-            >
-              <p>{item.description}</p>
+          {reminder.length > 0 && (
+            <ScrollArea className="h-[340px] lg:max-h-[calc(100vh - 15rem)] pr-0 w-hull flex-1">
+              {reminder.map((item) => (
+                <article
+                  key={item.id}
+                  className="flex flex-wrap flex-row items-center justify-between py-2 bg-muted mb-2 px-2 rounded-md"
+                >
+                  <p>{item.description}</p>
 
-              <Button variant="destructive" size="sm">
-                <Trash2 />
-              </Button>
-            </article>
-          ))}
+                  <Button
+                    onClick={() => handleDeleteReminder(item.id)}
+                    variant="destructive"
+                    size="sm"
+                  >
+                    <Trash2 />
+                  </Button>
+                </article>
+              ))}
+            </ScrollArea>
+          )}
         </CardContent>
       </Card>
     </div>
